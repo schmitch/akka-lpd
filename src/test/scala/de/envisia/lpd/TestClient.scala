c08255d6ebc8e6f5fc1ca1ca4183bafd32e745a9
Christian Schmitt <c.schmitt@briefdomain.de>
2017-01-24 15:23:46 +0100

    fail source if upstream failed

--- src/test/scala/de/envisia/lpd/TestClient.scala
+++ src/test/scala/de/envisia/lpd/TestClient.scala
@@ -15,8 +15,8 @@ object TestClient {
     implicit val system = ActorSystem()
     implicit val ec = system.dispatcher
     implicit val mat = ActorMaterializer(ActorMaterializerSettings(system))
-    val path = Paths.get("/Users/schmitch/sto/tmp/transformed_3290018523_WLT_WE_0456_cf572fb2.ps")
-    Await.result(new LpdClient().print("192.168.1.37", "schmitch", "print", path, "transformed_3290018523_WLT_WE_0456_cf572fb2.ps").recover {
+    val path = Paths.get("/Users/schmitch/sto/tmp/transformed_3290018523_WLT_WE_0456_f0e2c153.ps")
+    Await.result(new LpdClient().print("192.168.1.37", "schmitch", "print", path, "transformed_3290018523_WLT_WE_0456_f0e2c153.ps").recover {
       case e: Exception => e.printStackTrace(); Seq()
     }, 10.minutes)
     Await.result(system.terminate(), 10.minutes)
