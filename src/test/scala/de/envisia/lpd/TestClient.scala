ea0813ace4acfd7429965245ccefb9b8401b19ad
Christian Schmitt <c.schmitt@briefdomain.de>
2017-01-24 12:16:48 +0100

    fix build

--- src/test/scala/de/envisia/lpd/TestClient.scala
+++ src/test/scala/de/envisia/lpd/TestClient.scala
@@ -14,13 +14,10 @@ object TestClient {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
     implicit val ec = system.dispatcher
-    implicit val mat = ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy({ e =>
-      println(e)
-      Supervision.Stop
-    }))
+    implicit val mat = ActorMaterializer(ActorMaterializerSettings(system))
     val path = Paths.get("/Users/schmitch/sto/tmp/7440018391_WLT_WE_77330.ps")
     val size = Files.size(path)
-    Await.result(new LpdClient().print("172.16.206.100", "Cups-PDF", FileIO.fromPath(path, 4096), size, "7440018391_WLT_WE_77330.ps").recover {
+    Await.result(new LpdClient().print("172.16.206.100", "schmitch", "Cups-PDF", FileIO.fromPath(path, 4096), size, "7440018391_WLT_WE_77330.ps").recover {
       case e: Exception => e.printStackTrace(); Seq()
     }, 10.minutes)
     Await.result(system.terminate(), 10.minutes)
